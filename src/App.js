3a6229b2b238a035e41324c99219a6b14238ae9f
Benjamin Parish <parishben13@gmail.com>
2021-12-10 01:36:51 -0500

    importing API auth from gitignore file. Created initial function to fetch flight data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {amadeus} from './amadeusAPI';
 
 function App() {
+
+  let flightFetcher = () => {
+    amadeus.shopping.flightOffersSearch.get({
+      originLocationCode: 'SYD',
+      destinationLocationCode: 'BKK',
+      departureDate: '2021-12-20',
+      adults: '2'
+  }).then(function(response){
+    let holdIt = document.getElementById('flight-info-holder');
+    let flightArray = response.data;
+    let anArr = document.getElementById('arr');
+    console.log(response.data);
+  }).catch(function(responseError){
+    console.log(responseError.code);
+  });}
+
   return (
     <div className="App">
+        <div id='psuedo flight holder'>
+           <p id="flight-info-holder"></p>
+           <dd id="arr"></dd>
+        </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,6 +38,7 @@ function App() {
         >
           Learn React
         </a>
+        {flightFetcher()}
       </header>
     </div>
   );
